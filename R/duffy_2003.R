fd32b2748836f622bf5b5c97f5aa4f984009f3c2
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2013-10-17 12:23:29 -0400

    Adding Duffy et al. 2003 data

---
+++ R/duffy_2003.R
@@ -0,0 +1,9 @@
+#' @title Seagrass Mesocosm Data
+#' @description Data from the a seagrass grazer diversity manipulation at the Virginia Institute of Marine Sciences. From Duffy et al. 2003 Ecology Letters.
+#'
+#' @name duffy_2003
+#' @docType data
+#' @author Jarrett Byrnes
+#' @references Duffy, J. E., J. P. Richardson, and E. A. Canuel. 2003. Grazer diversity effects on ecosystem functioning in seagrass beds. Ecology letters 6:637–645.
+#' @keywords data
+NULL
