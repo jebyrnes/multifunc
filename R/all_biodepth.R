d99ac6ae7af47ddfe11de9c4a9a776f2135ccb3e
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2017-03-22 11:02:31 -0400

    fixed ascii

--- R/all_biodepth.R
+++ R/all_biodepth.R
@@ -7,7 +7,7 @@
 #' @references Spehn, E. M., A. Hector, J. Joshi, M. Scherer-Lorenzen, B. Schmid, 
 #' E. Bazeley-White, C. Beierkuhnlein, M. C. Caldeira,
 #' M. Diemer, P. G. Dimitrakopoulos, J. A. Finn, H. Freitas, P. S. Giller, 
-#' J. Good, R. Harris, P. Högberg, K. Huss-Danell, A. Jumpponen, J. Koricheva,
+#' J. Good, R. Harris, P. Hogberg, K. Huss-Danell, A. Jumpponen, J. Koricheva,
 #' P. W. Leadley, M. Loreau, A. Minns, C. P. H. Mulder, G. O'Donovan, S. J. Otway, 
 #' C. Palmborg, J. S. Pereira, A. B. Pfisterer, A. Prinz, D. J. Read, E. D. Schulze,
 #' A. S. Siamantziouras, A. C. Terry, A. Y. Troumbis, F. I. Woodward, S. Yachi, and J. H. Lawton. 2005. 
