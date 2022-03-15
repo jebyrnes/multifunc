c07b7e54d4b3ed19b6518320b00210195a833f41
jebyrnes <jarrett.byrnes@umb.edu>
2022-03-15 14:00:42 -0400

    cleaning namespace and old files

--- R/multifunc.R
+++ R/multifunc.R
@@ -3,7 +3,7 @@
 #' @name multifunc
 #' @docType package
 #' 
-#' @description Methods for the analysis of how ecological drivers affect the multifunctionality of an ecosystem.
+#' @description Methods for the analysis of how ecological drivers affect 
+#' the multifunctionality of an ecosystem.
 #' @author Jarrett Byrnes
-#' @import  dplyr car MASS tidyr stats util vegetarian
 #' NULL
